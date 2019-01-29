23acacf437f6c9e54f3605664742c255ade75c45
jakaperdana <kakjekofficial@gmail.com>
2019-01-29 17:28:11 +0700

    ver 1.0.0

--- test/unit/entry.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-require('angular');
-require('angular-mocks');
-require('../../src/entry');
-
-var testsContext = require.context('.', true, /\.spec/);
-testsContext.keys().forEach(testsContext);
